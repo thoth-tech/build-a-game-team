989c84fcb219f7c4483a582d298131a57f452dda
Anthony George <weareallone83@gmail.com>
2022-08-04 22:10:15 +1000

    feat: add enum and second overloaded constructor

--- block.h
+++ block.h
@@ -11,6 +11,15 @@ class Block
         string type;
         
     public:
+        /**
+         * @brief Enumeration of block style bitmaps
+         * 
+         */
+        enum BlockType
+        {
+            MARIO_BLOCKS
+        };
+
         Block(bitmap image, point_2d position)
         {
             this->image = image;
@@ -20,9 +29,26 @@ class Block
             this->top = position.y - 30;
             this->collision = position.y - 20;
         };
+
+        /**
+         * @brief Second Overloaded Constructor
+         * 
+         * @param BlockType 
+         * @param position 
+         */
+        Block(BlockType type, point_2d position)
+        {
+            this->image = bitmap_named(block_type(type));
+            this->position = position;
+            this->opts = option_defaults();
+
+            this->top = position.y - 30;
+            this->collision = position.y - 20;
+        };
+
         ~Block(){};
 
-        void draw_block()
+        virtual void draw_block()
         {
             draw_bitmap(image, position.x, position.y, opts);
         }
@@ -38,6 +64,32 @@ class Block
         {
             return this->top;
         }
+
+        point_2d get_pos()
+        {
+            return this->position;
+        }
+
+        /**
+         * @brief Add sprite sheets to this switch
+         * 
+         * Sprite sheets can be passed to the constructor by enum type
+         * 
+         * @param selection 
+         * @return string 
+         */
+        string block_type(BlockType selection)
+        {
+            switch(selection)
+            {
+                case MARIO_BLOCKS:
+                    return "MarioBlocks";
+                    break;
+                default:
+                    return "MarioBlocks";
+                    break;
+            }
+        }
 };
 
 class FloorBlock : public Block
@@ -46,6 +98,11 @@ class FloorBlock : public Block
         FloorBlock(bitmap image, point_2d position) : Block(image, position)
         {
             this->type = "Floor Block";
+            this->opts.draw_cell = 1;
+        }
+
+        FloorBlock(BlockType type, point_2d position) : Block(type, position)
+        {
             this->opts.draw_cell = 1;
         }
 };
@@ -58,6 +115,11 @@ class BrickBlock : public Block
             this->type = "Brick Block";
             this->opts.draw_cell = 0;
         }
+
+        BrickBlock(BlockType type, point_2d position) : Block(type, position)
+        {
+            this->opts.draw_cell = 0;
+        }
 };
 
 class QuestionBlock : public Block
@@ -68,4 +130,9 @@ class QuestionBlock : public Block
             this->type = "Question Block";
             this->opts.draw_cell = 2;
         }
+
+        QuestionBlock(BlockType type, point_2d position) : Block(type, position)
+        {
+            this->opts.draw_cell = 2;
+        }        
 };
