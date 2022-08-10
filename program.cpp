82f57863b91bc0ad5ff758bcb93a208dafb881da
lmorg42 <morganl@deakin.edu.au>
2022-08-10 12:06:36 +1000

    Added screen change system

--- program.cpp
+++ program.cpp
@@ -1,7 +1,6 @@
 #include "splashkit.h"
-#include "player.h"
-#include "level.h"
 #include "cellsheet.h"
+#include "screen.h"
 #include <memory>
 #include <vector>
 
@@ -27,34 +26,11 @@ vector<CellSheet> make_cell_sheets(vector<string> cell_sheet_names)
     return cell_sheets;
 }
 
-//Testing function for changing levels
-shared_ptr<Level> change_level(shared_ptr<Level> current_level, vector<CellSheet> cell_sheets, int players)
-{
-    //shared_ptr<Level> current_level;
-    if(key_typed(NUM_1_KEY))
-    {
-        shared_ptr<Level> level1(new Level1(cell_sheets, TILE_SIZE, players));
-        current_level = level1;
-    }
-
-    if(key_typed(NUM_2_KEY))
-    {
-        shared_ptr<Level> level2(new Level2(cell_sheets, TILE_SIZE, players));
-        current_level = level2;
-    }
-    
-
-    return current_level;
-}
-
 int main(int argc, char *argv[])
 {
     load_resource_bundle("player", "playerbundle.txt");
     load_resource_bundle("game_resources", "gameresources.txt");
     open_window("Platform Prototype", SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    bool testing = true;
-    bool custom_level = false;
     int players = 2;
 
     vector<string> cell_sheet_names;
@@ -67,9 +43,9 @@ int main(int argc, char *argv[])
     cell_sheet_names.push_back("Toxic");
 
     vector<CellSheet> cell_sheets = make_cell_sheets(cell_sheet_names);
-    shared_ptr<Level> current_level;
-
     std::vector<std::string> args(argv, argv+argc);
+    vector<string> files;
+
     try
     {
         for (size_t i = 1; i < args.size(); ++i) 
@@ -80,16 +56,8 @@ int main(int argc, char *argv[])
             }
             if (args[i] == "-l") 
             {
-                vector<string> files;
-
                 for(int j = 1; j < std::stoi(args[i + 1]) + 1; j++)
-                {
                     files.push_back(args[i + 1 + j]);
-                }
-
-                shared_ptr<Level> load(new BlankLevel(cell_sheets, TILE_SIZE, players, std::stoi(args[i + 1]), files));
-                current_level = load;
-                custom_level = true;
             }
         }
     }
@@ -100,24 +68,25 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if(!custom_level)
+    shared_ptr<Screen> screen;
+    if(files.size() != 0)
     {
-        shared_ptr<Level> level1(new Level1(cell_sheets, TILE_SIZE, players));
-        current_level = level1;
+        shared_ptr<Screen> custom_level(new Screen(new LevelScreen, TILE_SIZE, players, cell_sheets, files));
+        screen = custom_level;
+    }
+    else
+    {
+        shared_ptr<Screen> normal_game(new Screen(new LevelScreen, TILE_SIZE, players, cell_sheets));
+        screen = normal_game;
     }
     
     while (!key_typed(ESCAPE_KEY))
     {
-        current_level->update();
-
-        if(testing)
-            current_level = change_level(current_level, cell_sheets, players);
-
+        screen->update();
         process_events();
         refresh_screen(60);
     }
 
-    //delete player;
     free_resource_bundle("player");
     free_resource_bundle("game_resources");
     return 0;
