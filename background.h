442fb8c305ee941abb03b876da8122c6716cf17b
lmorg42 <morganl@deakin.edu.au>
2022-08-20 13:21:53 +1000

    Added Background + Refactors

---
+++ background.h
@@ -0,0 +1,29 @@
+#include "splashkit.h"
+#include <vector>
+
+class Background
+{
+    protected:
+        vector<bitmap> images;
+
+    public:
+        Background(){};
+        ~Background(){};
+
+        virtual void draw()
+        {
+            for(int i = 0; i < images.size(); i++)
+            {
+                draw_bitmap(images[i], 0, 0, option_to_screen());
+            }
+        };
+};
+
+class GreyBackground : public Background
+{
+    public:
+        GreyBackground()
+        {
+            images.push_back(bitmap_named("GreyBackground"));
+        }
+};
