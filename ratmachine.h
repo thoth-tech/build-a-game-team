1af1992aab73d00be146d66b4dbcc3b8c92ed7a7
Lily Lan <mlan@deakin.edu.au>
2022-09-23 19:33:20 +1000

    Added bite and crawl behaviour for rat enemy

--- ratmachine.h
+++ ratmachine.h
@@ -44,7 +44,7 @@ class RatMachine
         {
             this->enemy_sprite = enemy_sprite;
             this->level_players = level_players;
-            sprite_start_animation(enemy_sprite, "LeftRun");
+            sprite_start_animation(enemy_sprite, "LeftCrawl");
             this->change_state(state, "Idle");
         };
 
@@ -105,18 +105,116 @@ class RatMove : public RatMachineState
         void update() override;
 };
 
+class RatBite : public RatMachineState
+{
+    private:
+        bool run_once = false;
+
+    public:
+        RatBite(){};
+
+        ~RatBite(){};
+
+        void update() override;
+};
+
+class RatCrawl : public RatMachineState
+{
+    private:
+        bool run_once = false;
+
+    public:
+        RatCrawl(){};
+
+        ~RatCrawl(){};
+
+        void update() override;
+};
+
 void RatMove::update()
 {
     sprite rat_sprite = this->rat->get_sprite();
+    vector<std::shared_ptr<Player>> players = this->rat->get_level_players();
 
     if(this->rat->get_facing_left())
     {
-        sprite_set_dx(rat_sprite, 3);
+        sprite_set_dx(rat_sprite, 7);
         set_proper_direction(rat_sprite, "LeftRun");
     }
     else
     {
-        sprite_set_dx(rat_sprite, -3);
+        sprite_set_dx(rat_sprite, -7);
         set_proper_direction(rat_sprite, "RightRun");
+    }
+
+    for(int i = 0; i < players.size(); i++)
+    {
+        point_2d player_pos = to_screen(sprite_position(players[i]->get_player_sprite()));
+        point_2d rat_pos = to_screen(sprite_position(rat_sprite));
+
+        double x_dist = rat_pos.x - player_pos.x; 
+        if(abs(x_dist) > 500)
+        {
+            this->rat->change_state(new RatCrawl, "Crawl");
+            break;
+        }
+    }
+}
+
+void RatBite::update()
+{
+    sprite rat_sprite = this->rat->get_sprite();
+    vector<std::shared_ptr<Player>> players = this->rat->get_level_players();
+
+    sprite_set_dx(rat_sprite, 0);
+
+    if(this->rat->get_facing_left())
+        set_proper_direction(rat_sprite, "LeftBite");
+    else
+        set_proper_direction(rat_sprite, "RightBite");
+
+    for(int i = 0; i < players.size(); i++)
+    {
+        point_2d player_pos = to_screen(sprite_position(players[i]->get_player_sprite()));
+        point_2d rat_pos = to_screen(sprite_position(rat_sprite));
+
+        double x_dist = rat_pos.x - player_pos.x; 
+        double y_dist = rat_pos.y - player_pos.y;
+        if(abs(x_dist) > 18 && abs(y_dist) >17 )
+        {
+            this->rat->change_state(new RatMove, "Run");
+            break;
+        }
+    }
+}
+
+void RatCrawl::update()
+{
+    sprite rat_sprite = this->rat->get_sprite();
+    vector<std::shared_ptr<Player>> players = this->rat->get_level_players();
+
+    if(this->rat->get_facing_left())
+    {
+        sprite_set_dx(rat_sprite, 5);
+        set_proper_direction(rat_sprite, "LeftCrawl");
+    }
+    else
+    {
+        sprite_set_dx(rat_sprite, -5);
+        set_proper_direction(rat_sprite, "RightCrawl");
+    }
+
+    for(int i = 0; i < players.size(); i++)
+    {
+        point_2d player_pos = to_screen(sprite_position(players[i]->get_player_sprite()));
+        point_2d rat_pos = to_screen(sprite_position(rat_sprite));
+
+        double x_dist = rat_pos.x - player_pos.x; 
+        
+        if(abs(x_dist) < 20)
+        {
+            this->rat->change_state(new RatBite, "Bite");
+            break;
+        }
     }
 }
